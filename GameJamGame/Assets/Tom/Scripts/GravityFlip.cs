8dfd587a14b21c9103b20d11b6ca16bc15c3c20a
TomWoodley2 <Tom.woodley31@gmail.com>
2017-11-11 12:58:03 +0000

    Hiding upper/lower sections
    Box convers upper/lower section depending on which you are in

--- GameJamGame/Assets/Tom/Scripts/GravityFlip.cs
+++ GameJamGame/Assets/Tom/Scripts/GravityFlip.cs
@@ -16,26 +16,37 @@ public class GravityFlip : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        
+        Renderer ScreenUpperRenderer = GameObject.Find("ScreenUpper").GetComponent<Renderer>();
+        Renderer ScreenLowerRenderer = GameObject.Find("ScreenLower").GetComponent<Renderer>();
+        ScreenUpperRenderer.enabled = false;
     }
 
     private void OnTriggerExit(Collider collider)
     {
         PlayerMovement PlayerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
+        Renderer ScreenUpperRenderer = GameObject.Find("ScreenUpper").GetComponent<Renderer>();
+        Renderer ScreenLowerRenderer = GameObject.Find("ScreenLower").GetComponent<Renderer>(); 
+
         if (collider.name == "Player")
         {
             PlayerMovement.jumpHeight = -PlayerMovement.jumpHeight;
             if (gravity_IsFlipped == false)
-            
             {
                 Physics.gravity = new Vector3(0, 9.81f, 0);
-                
                 gravity_IsFlipped = true;
+
+                ScreenUpperRenderer.enabled = true;
+                ScreenLowerRenderer.enabled = false;
             }
             else
             {
                 gravity_IsFlipped = false;
                 Physics.gravity = new Vector3(0, -9.81f, 0);
+
+                ScreenUpperRenderer.enabled = false;
+                ScreenLowerRenderer.enabled = true;
+               
 
             }
         }
