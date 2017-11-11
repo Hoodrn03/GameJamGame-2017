992df2ca4086888998dcde283e0cc54c28ca0823
Hoodrn03 <Hoodrn03@gmail.com>
2017-11-11 15:20:54 +0000

    Keys and doors Implemented
    I have added a key spawning and worked on their interaction between the exit door

---
+++ GameJamGame/Assets/Scripts/SpawnKeys.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKeys : MonoBehaviour {
+
+
+	// Attatch the game manager to this to keep track of the number of keys.
+	[SerializeField]
+	private GameObject gameManager;
+
+	[SerializeField]
+	private GameObject key;
+
+	// Use this for initialization
+	void Start () {
+
+		// THis will spawn the keys at the very beggining of the game.
+		spawnKeys ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void spawnKeys()
+	{
+		// This will add to the total number of keys.
+		gameManager.GetComponent<TotalNumberOfKeys> ().addToNumberOfKeys ();
+
+		// THis will create a new instance of the key prefab. 
+		Instantiate (key, this.transform);
+	}
+		
+}
