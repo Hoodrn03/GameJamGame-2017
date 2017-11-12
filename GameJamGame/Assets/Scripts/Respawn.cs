83e13763efc154f3c32879f8dcb7e95f87d779ce
SparkyBS <P17206252@BDEXX214WIN037W.lec-admin.dmu.ac.uk>
2017-11-12 17:04:40 +0000

    Respawn.cs

---
+++ GameJamGame/Assets/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+	
+	void Start()
+	{
+
+
+	}
+	void Update()
+	{
+		var spawnpos = GameObject.Find("Spawn").transform.position;
+		if (Input.GetKeyDown("r")){
+			transform.localPosition = spawnpos; 
+		}
+	}
+
+
+}
